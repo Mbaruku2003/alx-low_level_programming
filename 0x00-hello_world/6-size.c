42f5642b52c79cdfd9ce7cec0424e657192cf938
Mbaruku2003 <leonardjembe21@gmail.com>
2023-02-19 11:23:41 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.c>
+/**
+ * main - Entry point of the program
+ * return: Always 0 (success)
+*/
+int main(void)
+{
+	printf(" prints the size of various types on the computer it is compiled\n");
+		return (0);
+}		
