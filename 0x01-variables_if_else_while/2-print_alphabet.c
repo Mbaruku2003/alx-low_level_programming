172ed7bda31bbef58ae6957af0740d4c884e97b5
Mbaruku2003 <leonardjembe21@gmail.com>
2023-02-20 12:18:36 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
