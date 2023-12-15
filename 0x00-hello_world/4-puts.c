448310b7fdc231c29f2bea215d21cbb5a649de96
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-15 07:43:29 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a sentence followed by a line
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
