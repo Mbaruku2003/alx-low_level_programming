d3216c17c808126e15a5bb5dcac0c3b1e6804202
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-16 12:15:07 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 	char a;
 	char b;
 
-	for (a = 30; a <= 39; a++)
+	for (a = 48; a <= 57; a++)
 	{
 		putchar(a);
 	}
-	for (b = 97; b <= 122; b++)
+	for (b = 97; b <= 102; b++)
 	{
 		putchar(b);
 	}
