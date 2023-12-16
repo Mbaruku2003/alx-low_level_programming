e2d1cefa425a01409d61e70fc12172661c4b9c74
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-16 12:54:41 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,12 @@ int main(void)
 	for (a = 48; a <= 57; a++)
 	{
 		putchar(a);
-		if (a != 57)
+		if (a < 57)
 		{
 		putchar(',');
 		putchar(' ');
 		}
-		else
-		{
-			putchar(a + '\n');
-		}
 	}
+	putchar('\n');
 	return (0);
 }
