21ad06196875af1cd738881f4772cd9839f50d85
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-19 02:34:34 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - checks for lowercase character
+ * Return: 1 (if c is lower), 0 (otherwise)
+ */
+int _islower(int c)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		return (1)
+	}
+	return (0);
+}
