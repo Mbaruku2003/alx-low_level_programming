34d5b13034122d9d4e21c06724f1964371c2aa33
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-19 08:30:51 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @m: character being tested for absolute value
+ * Return: 0(success)
+ */
+int _abs(int m)
+{
+	if (m < 0)
+	{
+		m = m * -1;
+	}
+	return (0);
+}
