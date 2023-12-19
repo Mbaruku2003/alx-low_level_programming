c91a9da3a6730e2be13782f339a9394810cfed78
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-19 02:10:00 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * return: Always 0(success)
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	char b;
+
+	for (a = 48; a<= 57; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			putchar(b);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
