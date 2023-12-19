1bb7ccb3d3134b402fa12e5392b6ffcf10316975
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-19 10:14:14 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,13 @@ int print_last_digit(int n)
 	int m;
 
 	m = n % 10;
-	if (n < 0)
+	if (m >= 0)
 	{
-		m = m * -1;
 		_putchar(m);
 	}
-	_putchar(m);
+	else
+	{
+		_putchar(m);
+	}
 	return (m);
 }
