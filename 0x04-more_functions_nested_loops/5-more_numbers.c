cd95df1f1e0b3f7dbc0f691fc031e51cb11bb130
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-22 02:03:01 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,13 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j <= 9)
 			{
-				_putchar(i + '0');
+				_putchar(j + '0');
 			}
-			else
+			else if (j > 9)
 			{
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
@@ -25,4 +25,3 @@ void more_numbers(void)
 		_putchar('\n');
 	}
 }
-		
