d15c98e5219eb9259106aaf5ffa02e29e700ee0b
Mbaruku2003 <leonardjembe21@gmail.com>
2023-12-22 01:18:35 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,21 +7,16 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 14; j++)
 		{
-			_putchar(j);
 			if (j > 9)
 			{
-				k = j % 10;
-				l = j / 10;
-				_putchar(l + '0');
-				_putchar(k + '0');
+				_putchar((j / 10) + '0');
 			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
