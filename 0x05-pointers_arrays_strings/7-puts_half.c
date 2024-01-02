496c383f3daaae94888544300f9da43a5071fad5
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-02 07:31:18 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,11 @@
 void puts_half(char *str)
 {
 	int i;
-	int count;
+	int count = 0;
 	int n;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		count++;
-	}
 	n = (count - 1) / 2;
 
 	for (i = n + 1; str[i] != '\0'; i++)
