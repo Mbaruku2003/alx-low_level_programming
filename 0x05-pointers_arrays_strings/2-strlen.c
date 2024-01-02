e9ad44b22dbcd50fcbf795ca7f5b92190b0799b8
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-01 23:49:45 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,15 @@
 /**
  * _strlen - returns the length of a string
  * @s: string for which we are finding out the length of
- * Return: 0(success)
+ * Return: length(success)
  */
 int _strlen(char *s)
 {
 	int length = 0;
-
-	if (*s != '\0')
-	{
-		s++;
-		length++;
-	}
+	while (*s++)
+		if (*s != '\0')
+		{
+			length++;
+		}
 	return (length);
 }
