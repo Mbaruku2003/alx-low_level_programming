d53a5dd2773d660e88d353ed9826c73abd58ee4a
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-04 13:56:43 -0800

    whats the problem

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,9 +14,9 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] != s2[i])
 		{
 			if (s1[i] < s2[i])
-				return (s2[i] - s1[i]);
-			if (s2[i] < s1[i])
-				return (s2[i] - s1[i]);
+				return (s1[i] - s2[i]);
+			else if (s2[i] < s1[i])
+				return (s1[i] - s2[i]);
 		}
 	}
 	return (0);
