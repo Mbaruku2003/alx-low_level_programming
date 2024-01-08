a90724a2b90d7b5c89ac071db03f5cefdb65fb4a
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-08 12:47:13 -0800

    we are counting for each index the character is not similar to that in accept

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substrin
+ * @s: initial segment which only of bytes from accept
+ * Return: (0);
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[i] != accept[j]; j++)
+		{
+			if (accept[j] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (0);
+}
