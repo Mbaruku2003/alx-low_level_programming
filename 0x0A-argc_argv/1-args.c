395e81c7b52087718d45d7ec4830e9f7b3f50e7e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-12 03:14:41 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main: Entry point of the program
+ * @argc: counts the number of arguenments
+ * @argv: string for the command line arguenment
+ * Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+		printf("%d\n", argc - 1);
+	return (0);
+}
