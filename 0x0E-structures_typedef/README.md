8f15a066108df4232daf26804924dc9f34705593
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-22 01:27:21 -0800

    to make the header files include guarded we use the #ifndef ,define and endif in the code

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+structs allow us to group a number of types of variables together
+typedefs are used to give a new name to a variable type
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dog - defines a new type
+ * @name: frst member
+ * @age: second member
+ * @owner:third member
+ * Description: defines a new type struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
