e32088b5068fb187a575fc6b0049562bca7ac023
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-22 03:12:23 -0800

    wtf

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /*
  * print_dog - prints the details f a dog
- * @d - the dog
+ * @d: the dog
  * Return : void
  */
 void print_dog(struct dog *d)
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("name: %s\n", d->name);
+		printf("Name: %s\n", d->name);
 	}
 	if (d->age < 0)
 	{
@@ -23,10 +23,10 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("age: %.6f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 	}
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		printf("owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 }
