5aaafad45069a7d2d41e90f9dd3eb7e26d3a7dbb
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-22 02:08:50 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog
+	if (d != NULL)
 	{
-		char *name;
-		float age;
-		char owner;
-	}*d;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
