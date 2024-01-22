13ac977d70b560bfbbbcfd84a7acefbdb37492fb
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-22 11:16:01 -0800

    freeing means freeing everything

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees a dog
+ * @d: pointer to dog_t
+ * Return : void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
