dc80f280b7b02dc4cf3788b17490b20ed8c6411f
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-23 11:36:38 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ * Return: 0(success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+		return(0);
+}
