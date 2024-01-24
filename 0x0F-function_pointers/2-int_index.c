e7f33d6abc519ab1bb8eeec282657b35b307667b
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-24 12:14:35 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * array_iterator - executes a function given as a parameter on each element
+ * size: the number of elements in the arrray
+ * cmp: pointer to the function to be used to declare variables
+ * array: holds the number of elements
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		return (i);
+	}
+	return (0);
+}
