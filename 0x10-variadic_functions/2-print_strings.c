7dfdfd72e22cedaa7a5c7e33eec4d018f91d4346
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-25 08:43:23 -0800

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * prints_strings - prints strings followed by a new line
+ * print_strings - prints strings followed by a new line
  * @separator: the string to be printed betweeen the strings
  * @n: the number of strings to be passed to the function
  * Return: void
@@ -14,23 +14,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_list mbaruku;
 	{
-		va_start(mbaruku, n);
+	va_start(mbaruku, n);
+	for (i = 0; i < n; i++)
+	{
+		leo = va_arg(mbaruku, char *);
+		if (leo == NULL)
 		{
-			for (i = 0; i < n; i++)
-			{
-				leo = va_arg(mbaruku, char *);
-				if (leo == NULL)
-				{
-					printf("(nil)");
-				}
-				else
-					printf("%s", leo);
-				if ( i  !=  (n - 1) || separator != NULL)
-				{
-					printf("%s", separator);
-				}
-			}
+			printf("(nil)");
 		}
+		else
+			printf("%s", leo);
+		if (i  !=  (n - 1) || separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		if (separator == NULL)
+			return;
+	}
 	}
 	printf("\n");
 	va_end(mbaruku);
