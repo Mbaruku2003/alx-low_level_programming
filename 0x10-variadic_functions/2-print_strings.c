94fa0c60901ada3b345a6a3d8f8847013aa7efcf
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-25 08:17:26 -0800

    mention of comparison, for a null pointer is important

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * prints_strings - prints strings followed by a new line
+ * @separator: the string to be printed betweeen the strings
+ * @n: the number of strings to be passed to the function
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *leo;
+
+	va_list mbaruku;
+	{
+		va_start(mbaruku, n);
+		{
+			for (i = 0; i < n; i++)
+			{
+				leo = va_arg(mbaruku, char *);
+				if (leo == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+					printf("%s", leo);
+				if ( i  !=  (n - 1) || separator != NULL)
+				{
+					printf("%s", separator);
+				}
+			}
+		}
+	}
+	printf("\n");
+	va_end(mbaruku);
+}
