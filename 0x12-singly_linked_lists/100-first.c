f0decd040152d5ddd3ef28eef6eabfecf8036ecf
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-01 13:08:18 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * new - prints a segment before the main
+ * function is executed
+ */
+void new(void)
+{
+	printf("Youre beat! and yet, you must allow.\n");
+	printf("I bore my house upon my back!\n(;
+}
