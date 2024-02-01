154b7c08dd7d10bd2f68e63ae47b4db6dc0763a9
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-01 13:25:05 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 void first(void)__attribute__((constructor));
 /**
- * new - prints a segment before the main
+ * first - prints a segment before the main
  * function is executed
  */
 void first(void)
