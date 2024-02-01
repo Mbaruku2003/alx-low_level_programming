4f65d766b6a78dfea64d6cfd5ac8f552fd02e168
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-01 13:22:43 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@ void first(void)__attribute__((constructor));
  * new - prints a segment before the main
  * function is executed
  */
-void new(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
