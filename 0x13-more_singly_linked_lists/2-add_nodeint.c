a2fe18c12e2d7716271735fd3a4ce89c82eaa5db
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-05 13:32:32 -0800

    ive tried to rearrange my stream of thought

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_nodeint - adds a new node to the beggining of listnt
+ * @head: the header file
+ * @n: the number of integers
+ * Return: the addresss of a new element or NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *leo;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	leo = malloc(sizeof(struct listint_s));
+	if (leo == NULL)
+	{
+		return (NULL);
+	}
+	if(*head == NULL)
+	{
+		leo->next = NULL
+	}
+	else
+	{
+		leo->next = *head;
+		leo->n = n;
+		*head = leo;
+	}
+	return (*head);
+}
