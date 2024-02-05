9f7bd71894dd9bce74c2660bc92605e37f975194
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-05 07:41:03 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,15 @@
 size_t print_listint(const listint_t *h)
 {
 	int count =  0;
+	int i;
 
-	while (h)
+	if (h == NULL)
 	{
-		printf("%d\n", h->n);
+		return (0);
+	}
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d", h->n);
 		count++;
 		h = h->next;
 	}
