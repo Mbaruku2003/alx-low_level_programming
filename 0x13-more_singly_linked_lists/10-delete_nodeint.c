3d956320032e9435f5cd63057ff7b44a92c157fe
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-06 14:00:22 -0800

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return (-1);
 	for (i = 0; i < index - 1 && temporary != NULL && index != 0; i++)
-		temporary = head->next;
+		temporary = temporary->next;
 	if (temporary == NULL)
 		return (-1);
 	if (index == 0)
