5968a8e56a6cc511b1e39f4df63b81775c0f2fd4
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-21 07:21:21 -0700

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,14 +9,13 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *iterator;
-	iterator = head;
 
 	if (head != NULL)
 	{
 		while (head->prev != NULL)
 			head = head->prev;
 	}
-	while((iterator = head) != NULL)
+	while ((iterator = head) != NULL)
 	{
 		head = head->next;
 		free(iterator);
