1d981be95a22ee864690c450e4c8e0779e023c90
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-12 11:28:04 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 #include "hash_tables.h"
 /**
  * hash_table_create - creates a hash table
- * @size - the sie of the array
+ * @size: the sie of the array
  * Return: a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t*));
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
