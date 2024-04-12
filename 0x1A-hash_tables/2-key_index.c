cdfcd155bd15e6b254df1d4689cf89529ec6ae48
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-12 11:22:55 -0700

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * key_index - the key index
+ * @key - the key
+ * @size - the size
+ * Return: the index for storing the key valuse pair
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
