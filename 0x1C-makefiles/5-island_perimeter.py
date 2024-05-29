aabe50a7e2034309dae4aea75128c16f9c1f4d55
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-29 06:13:15 -0700

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Returns the perimeter of the island described in grid."""
+
+
+def island_perimeter(grid):
+    """Zeo represents a water zone while 1 an zone."""
+
+    height =len(grid)
+    width = len(grid[0])
+    size = 0
+    edge = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edge += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edge += 1
+    return (size * 4) - (edge * 2)
